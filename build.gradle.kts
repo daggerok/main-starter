20ecec38cea64ade8cfd9f64762499e2da216aab
Maksim Kostromin <daggerok@gmail.com>
2019-04-06 21:17:51 +0300

    Add Kotlin config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   java
   application
+  kotlin("jvm") version "1.3.21"
+  kotlin("plugin.spring") version "1.3.21"
   id("io.franzbecker.gradle-lombok") version "2.1"
 }
 
@@ -17,9 +21,27 @@ tasks.withType(Wrapper::class.java) {
   distributionType = Wrapper.DistributionType.BIN
 }
 
+sourceSets {
+  main {
+    java.srcDir("src/main/kotlin")
+  }
+  test {
+    java.srcDir("src/test/kotlin")
+  }
+}
+
+val javaVersion = JavaVersion.VERSION_1_8
+
+tasks.withType<KotlinCompile>().configureEach {
+  kotlinOptions {
+    freeCompilerArgs += "-Xjsr305=strict"
+    jvmTarget = "$javaVersion"
+  }
+}
+
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = javaVersion
+  targetCompatibility = javaVersion
 }
 
 repositories {
@@ -33,9 +55,11 @@ lombok {
 }
 
 dependencies {
+  implementation(kotlin("stdlib"))
+  implementation(kotlin("reflect"))
   implementation("org.jboss.weld.se:weld-se-core:3.1.0.Final")
   implementation("javax.enterprise:cdi-api:2.0")
-  implementation("org.jboss:jandex:2.0.5.Final")
+  implementation("org.jboss:jandex:2.1.1.Final")
   implementation("org.slf4j:slf4j-api:1.7.26")
   implementation("ch.qos.logback:logback-classic:1.2.3")
   annotationProcessor("org.projectlombok:lombok:$lombokVersion")
