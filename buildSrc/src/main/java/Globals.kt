45b70d1e6b2452f7aa69c2152e13b7bef0289e1d
Maksim Kostromin <daggerok@gmail.com>
2019-05-26 07:55:49 +0300

    Update versions! | add dirty scripts from the dark side of the universe :) (C) whatthecommit.com

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -4,19 +4,8 @@ object Globals {
   object Project {
     const val artifactId = "main-starter"
     const val groupId = "com.github.daggerok"
-    const val version = "1.1-SNAPSHOT"
+    const val version = "1.2-SNAPSHOT"
     const val mainClass = "com.github.daggerok.App"
-  }
-
-  object Spring {
-    const val springBootVersion = "2.1.5.RELEASE"
-    const val springDataR2dbcVersion = "1.0.0.M1"
-    const val springFuJafuVersion = "0.0.5"
-  }
-
-  object R2dbc {
-    const val r2dbcH2Version = "1.0.0.BUILD-SNAPSHOT"
-    const val r2dbcSpiVersion = "1.0.0.M7"
   }
 
   val javaVersion = JavaVersion.VERSION_1_8
@@ -40,7 +29,6 @@ object Globals {
     object Plugin {
       const val lombokVersion = "3.6.4"
       const val versionsVersion = "0.21.0"
-      const val dependencyManagementVersion = "1.0.7.RELEASE"
     }
   }
 }
