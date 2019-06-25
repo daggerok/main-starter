c2ed755ac4be3ef099508bb32c70f325ba70c8ca
Maksim Kostromin <daggerok@gmail.com>
2019-06-26 02:15:42 +0300

    Update versions.

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -4,7 +4,7 @@ object Globals {
   object Project {
     const val artifactId = "main-starter"
     const val groupId = "com.github.daggerok"
-    const val version = "1.3-SNAPSHOT"
+    const val version = "1.4-SNAPSHOT"
     const val mainClass = "com.github.daggerok.App"
   }
 
@@ -12,14 +12,14 @@ object Globals {
 
   const val cdiApiVersion = "2.0"
   const val vavrVersion = "0.10.0"
-  const val kotlinVersion = "1.3.31"
+  const val kotlinVersion = "1.3.40"
   const val lombokVersion = "1.18.8"
   const val assertjVersion = "3.12.2"
   const val weldVersion = "3.1.1.Final"
-  const val slf4jVersion = "1.8.0-beta4"
+  const val slf4jVersion = "2.0.0-alpha0"
   const val jandexVersion = "2.1.1.Final"
   const val logbackVersion = "1.3.0-alpha4"
-  const val junitJupiterVersion = "5.5.0-RC1"
+  const val junitJupiterVersion = "5.5.0-RC2"
 
   object Gradle {
     const val wrapperVersion = "5.5-rc-2"
