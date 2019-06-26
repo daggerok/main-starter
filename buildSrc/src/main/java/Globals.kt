9af0c9c006b672dccf0db1223f23ac0953a08ab6
Maksim Kostromin <daggerok@gmail.com>
2019-06-26 05:05:51 +0300

    Update.

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -4,24 +4,24 @@ object Globals {
   object Project {
     const val artifactId = "main-starter"
     const val groupId = "com.github.daggerok"
-    const val version = "1.2-SNAPSHOT"
+    const val version = "1.3-SNAPSHOT"
   }
 
   val javaVersion = JavaVersion.VERSION_1_8
   const val vavrVersion = "0.10.0"
-  const val kotlinVersion = "1.3.31"
+  const val kotlinVersion = "1.3.40"
   const val lombokVersion = "1.18.8"
   const val junitVersion = "4.13-beta-3"
-  const val junitJupiterVersion = "5.5.0-RC1"
+  const val junitJupiterVersion = "5.5.0-RC2"
 
   object Gradle {
-    const val wrapperVersion = "5.5-rc-2"
+    const val wrapperVersion = "5.5-rc-4"
 
     object Plugin {
       const val lombokVersion = "3.1.0"
       const val versionsVersion = "0.21.0"
-      const val springBootVersion = "2.2.0.M3"
-      const val dependencyManagementVersion = "1.0.7.RELEASE"
+      const val springBootVersion = "2.2.0.M4"
+      const val dependencyManagementVersion = "1.0.8.RELEASE"
     }
   }
 }
