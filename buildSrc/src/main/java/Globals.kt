6aa8f61711371b8e279e754ded85ee6f07a27943
Maksim Kostromin <daggerok@gmail.com>
2019-07-28 03:31:29 +0300

    Update versions. | Can someone review this commit, please ? (C) whatthecommit.com

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -4,7 +4,7 @@ object Globals {
   object Project {
     const val artifactId = "main-starter"
     const val groupId = "com.github.daggerok"
-    const val version = "1.4-SNAPSHOT"
+    const val version = "1.5-SNAPSHOT"
     const val mainClass = "com.github.daggerok.App"
   }
 
@@ -12,20 +12,20 @@ object Globals {
 
   const val cdiApiVersion = "2.0"
   const val vavrVersion = "0.10.0"
-  const val kotlinVersion = "1.3.40"
+  const val kotlinVersion = "1.3.41"
   const val lombokVersion = "1.18.8"
   const val assertjVersion = "3.12.2"
   const val weldVersion = "3.1.1.Final"
   const val slf4jVersion = "2.0.0-alpha0"
   const val jandexVersion = "2.1.1.Final"
   const val logbackVersion = "1.3.0-alpha4"
-  const val junitJupiterVersion = "5.5.0-RC2"
+  const val junitJupiterVersion = "5.5.1"
 
   object Gradle {
-    const val wrapperVersion = "5.5-rc-4"
+    const val wrapperVersion = "5.5.1"
 
     object Plugin {
-      const val lombokVersion = "3.6.6"
+      const val lombokVersion = "3.8.1"
       const val versionsVersion = "0.21.0"
     }
   }
