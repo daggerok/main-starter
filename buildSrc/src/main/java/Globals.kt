26277a2568af07afaea8015ace8e478452c779e3
Maksim Kostromin <daggerok@gmail.com>
2019-08-03 00:11:42 +0300

    Update Globals.kt

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -4,11 +4,11 @@ object Globals {
   object Project {
     const val artifactId = "main-starter"
     const val groupId = "com.github.daggerok"
-    const val version = "1.1-SNAPSHOT"
+    const val version = "1.3-SNAPSHOT"
   }
 
   object Spring {
-    const val springBootVersion = "2.1.5.RELEASE"
+    const val springBootVersion = "2.1.6.RELEASE"
     const val springDataR2dbcVersion = "1.0.0.M1"
     const val springFuJafuVersion = "0.0.5"
   }
@@ -20,15 +20,15 @@ object Globals {
 
   val javaVersion = JavaVersion.VERSION_1_8
   const val lombokVersion = "1.18.8"
-  const val vavrVersion = "0.10.0"
+  const val vavrVersion = "0.10.1"
 
   object Gradle {
-    const val wrapperVersion = "5.4.1"
+    const val wrapperVersion = "5.6-rc-1"
 
     object Plugin {
       const val lombokVersion = "3.6.1"
       const val versionsVersion = "0.21.0"
-      const val dependencyManagementVersion = "1.0.7.RELEASE"
+      const val dependencyManagementVersion = "1.0.8.RELEASE"
     }
   }
 }
