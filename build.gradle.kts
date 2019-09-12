3de7f71549a5dc4a19f5cdf39d9bbedf5e390b8a
Maksim Kostromin <daggerok@gmail.com>
2019-09-12 22:02:56 +0300

    Update versions... | that's all folks (C) whatthecommit.com

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,10 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 plugins {
   java
   application
-  kotlin("jvm") version "1.3.31"
-  kotlin("plugin.spring") version "1.3.31"
-  id("io.franzbecker.gradle-lombok") version "3.0.0"
-}
-
-tasks.withType(Wrapper::class.java) {
-  val gradleWrapperVersion: String by project
-  gradleVersion = gradleWrapperVersion
-  distributionType = Wrapper.DistributionType.BIN
+  kotlin("jvm") version "1.3.50"
+  kotlin("plugin.spring") version "1.3.50"
+  id("io.franzbecker.gradle-lombok") version "3.1.0"
+  id("com.github.ben-manes.versions") version "0.25.0"
 }
 
 sourceSets {
@@ -25,14 +20,6 @@ sourceSets {
 }
 
 val javaVersion = JavaVersion.VERSION_1_8
-
-tasks.withType<KotlinCompile>().configureEach {
-  kotlinOptions {
-    freeCompilerArgs += "-Xjsr305=strict"
-    jvmTarget = "$javaVersion"
-  }
-}
-
 java {
   sourceCompatibility = javaVersion
   targetCompatibility = javaVersion
@@ -42,23 +29,23 @@ repositories {
   mavenCentral()
 }
 
-val lombokVersion: String by project
-
 lombok {
+  val lombokVersion: String by project
   version = lombokVersion
 }
 
-val weldVersion: String by project
-val cdiApiVersion: String by project
-val jandexVersion: String by project
-val slf4jVersion: String by project
-val logbackVersion: String by project
-val junit4Version: String by project
-val assertkVersion: String by project
-val assertjVersion: String by project
-val junitJupiterVersion: String by project
-
 dependencies {
+  val weldVersion: String by project
+  val slf4jVersion: String by project
+  val cdiApiVersion: String by project
+  val jandexVersion: String by project
+  val junit4Version: String by project
+  val lombokVersion: String by project
+  val logbackVersion: String by project
+  val assertkVersion: String by project
+  val assertjVersion: String by project
+  val junitJupiterVersion: String by project
+
   implementation(kotlin("stdlib"))
   implementation(kotlin("reflect"))
   //implementation("io.vavr:vavr:0.10.0")
@@ -79,27 +66,37 @@ dependencies {
   testRuntime("org.junit.platform:junit-platform-launcher")
 }
 
-val mainClass: String by project
-
 application {
+  val mainClass: String by project
   mainClassName = mainClass
 }
 
-tasks.withType<Test> {
-  useJUnitPlatform()
-  testLogging {
-    showExceptions = true
-    showStandardStreams = true
-    events(PASSED, SKIPPED, FAILED)
-  }
-}
-
 tasks {
+  withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+      freeCompilerArgs += "-Xjsr305=strict"
+      jvmTarget = "$javaVersion"
+    }
+  }
+  withType(Wrapper::class.java) {
+    val gradleWrapperVersion: String by project
+    gradleVersion = gradleWrapperVersion
+    distributionType = Wrapper.DistributionType.BIN
+  }
+  withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+      showExceptions = true
+      showStandardStreams = true
+      events(PASSED, SKIPPED, FAILED)
+    }
+  }
   register("fatJar", Jar::class.java) {
     //archiveAppendix.set("all")
     archiveClassifier.set("all")
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     manifest {
+      val mainClass: String by project
       attributes("Main-Class" to mainClass)
     }
     from(configurations.runtimeClasspath.get()
@@ -109,27 +106,23 @@ tasks {
     sourcesMain.allSource.forEach { println("add from sources: ${it.name}") }
     from(sourcesMain.output)
   }
-}
-
-tasks.create<Zip>("sources") {
-  dependsOn("clean")
-  shouldRunAfter("clean", "assemble")
-  description = "Archives sources in a zip file"
-  group = "Archive"
-  from("src") {
-    into("src")
+  create<Zip>("sources") {
+    dependsOn("clean")
+    shouldRunAfter("clean", "assemble")
+    description = "Archives sources in a zip file"
+    group = "Archive"
+    from("src") {
+      into("src")
+    }
+    from(".gitignore")
+    from(".java-version")
+    from(".travis.yml")
+    from("build.gradle.kts")
+    from("pom.xml")
+    from("README.md")
+    from("settings.gradle.kts")
+    archiveFileName.set("${project.buildDir}/sources-${project.version}.zip")
   }
-  from(".gitignore")
-  from(".java-version")
-  from(".travis.yml")
-  from("build.gradle.kts")
-  from("pom.xml")
-  from("README.md")
-  from("settings.gradle.kts")
-  archiveFileName.set("${project.buildDir}/sources-${project.version}.zip")
-}
-
-tasks {
   named("clean") {
     doLast {
       delete(
