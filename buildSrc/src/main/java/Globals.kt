5784ad6890d4379f1567b23beeafb054b9e2a828
Maksim Kostromin <daggerok@gmail.com>
2019-12-14 20:48:47 +0200

    perfect... (C) whatthecommit.com

--- buildSrc/src/main/java/Globals.kt
+++ buildSrc/src/main/java/Globals.kt
@@ -11,21 +11,21 @@ object Globals {
 
   const val cdiApiVersion = "2.0"
   const val vavrVersion = "0.10.2"
-  const val kotlinVersion = "1.3.50"
+  const val kotlinVersion = "1.3.61"
   const val lombokVersion = "1.18.10"
-  const val assertjVersion = "3.13.2"
-  const val weldVersion = "3.1.2.Final"
-  const val slf4jVersion = "2.0.0-alpha0"
-  const val jandexVersion = "2.1.1.Final"
-  const val logbackVersion = "1.3.0-alpha4"
-  const val junitJupiterVersion = "5.5.2"
+  const val assertjVersion = "3.14.0"
+  const val weldVersion = "3.1.3.Final"
+  const val slf4jVersion = "2.0.0-alpha1"
+  const val jandexVersion = "2.1.2.Final"
+  const val logbackVersion = "1.3.0-alpha5"
+  const val junitJupiterVersion = "5.6.0-M1"
 
   object Gradle {
-    const val wrapperVersion = "5.6.2"
+    const val wrapperVersion = "5.6.4"
 
     object Plugin {
-      const val lombokVersion = "3.1.0"
-      const val versionsVersion = "0.25.0"
+      const val lombokVersion = "3.2.0"
+      const val versionsVersion = "0.27.0"
     }
   }
 }
